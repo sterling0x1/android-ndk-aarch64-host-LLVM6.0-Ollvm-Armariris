a76a9cac42d3dd6c664e0e819cf1e56744aab63f
Sterling0x1 <sterling0x1@protonmail.com>
2020-09-15 13:52:27 +0000

    test/

---
+++ test/x32/jni/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+MAIN_LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := jmtrepublic
+
+LOCAL_CPPFLAGS := -std=c++11
+LOCAL_CFLAGS := -Wno-error=format-security -fpermissive -Wc++11-extensions -Wno-absolute-value
+LOCAL_CFLAGS += -fno-rtti -fno-exceptions
+
+LOCAL_C_INCLUDES += $(MAIN_LOCAL_PATH)
+
+LOCAL_SRC_FILES := src/main.cpp \
+				   libs/Substrate/hde64.c \
+                   libs/Substrate/SubstrateDebug.cpp \
+                   libs/Substrate/SubstrateHook.cpp \
+                   libs/Substrate/SubstratePosixMemory.cpp \
+				   libs/KittyMemory/KittyMemory.cpp \
+				   libs/KittyMemory/MemoryPatch.cpp \
+
+LOCAL_LDLIBS := -llog
+
+include $(BUILD_SHARED_LIBRARY)
